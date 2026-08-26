114a0442cae0b1dd9ee2dc3863ec12ee8c05e459
Nathan JAUNET <njaunet@datasolution.fr>
2026-08-26 11:52:39 +0200

    Pin Bun to 1.4.0 and raise HTTP-budget test timeouts

--- spores/plex/test/plex.test.ts
+++ spores/plex/test/plex.test.ts
@@ -294,7 +294,7 @@ describe('the plex http budget', () => {
     } finally {
       await hanging.stop(true)
     }
-  }, 1000)
+  }, 20000) // full-suite load flakes a 1000 ms test timeout even though the abort budget itself is 50 ms
 })
 
 describe('the plex spore', () => {
